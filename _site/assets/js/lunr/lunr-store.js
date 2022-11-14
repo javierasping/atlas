9ff0fffe6e0e0d82a0aad2137c3ce92bd98eab83
javierasping <javierasping@gmail.com>
2022-11-14 15:45:06 +0100

    tercer commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,6 +4,11 @@ var store = [{
         "tags": ["SMR"],
         "url": "http://localhost:4000/servicios-win-server-dhcp-dns-iss-rdp-ftp/",
         "teaser":"http://localhost:4000/assets/images/prueba/logo.png"},{
+        "title": "Politica_pyme",
+        "excerpt":"Ejemplo plan director de seguridad Introducción El sector de la educación engloba todo tipo de organizaciones que se dedican a la enseñanza . La implantación de nuevas tecnologías es importante en este tipo de instituciones y empresas . En las cuales se hace un uso intensivo de equipos informáticos en...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/politica_pyme/",
+        "teaser":null},{
         "title": "Scripts administracion usuarios y grupos locales  active directory",
         "excerpt":"Usuarios y grupos locales Crear un script de PowerShell que realice las siguientes tareas (script1.ps1) Crear un usuario llamado “usuloc01”, sin contraseña y que esté deshabilitado. Habilitar el usuario “usuloc01” Modificar la descripción del usuario “usuloc01” para que tenga el texto “Descripción usuario de prueba”. Crear otro usuario llamado “usuloc02”...","categories": ["Sisteamas"],
         "tags": ["SMR"],
